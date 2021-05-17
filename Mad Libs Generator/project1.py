d296aac57bb6e4fbc0725f3481c0491ce87b8fe0
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-05-17 20:40:06 +0530

    Create project1.py

---
+++ Mad Libs Generator/project1.py
@@ -0,0 +1,19 @@
+
+loop = 1
+while (loop < 10):
+
+    place = input("Choose a place: ")
+    adjective = input("Choose a adjective: ")
+    adjective2 = input("Choose a adjective2: ")
+    name = input("Write a name: ")
+   
+    
+
+    print ("------------------------------------------")
+    print ("I love to visit",place,"Dude! it's so freaking", adjective,".")
+    print ("The weather there is so",adjective2,"everyday.")
+    print ("You know last time I went there was with",name,"and that person totally went insane.")
+    print ("Oh God, if there is anywhere heaven on this earth, it totally lies in",place,"!!")
+    print ("------------------------------------------")
+
+    loop = loop + 1
